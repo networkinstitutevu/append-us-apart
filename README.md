8ac59f9b44cab7259921ca7c0eecff8c8ccb5ed4
Raiduy <radu.apsan@gmail.com>
2023-07-26 16:27:07 +0200

    Add readme, code, and photos

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # append-us-apart
 A non-humanoid embodied AI-installation where a user can interact with an LLM through voice prompting and getting their answers on paper as to show the effects of LLM-output on the physical world
+
+
+## Context
+30 November 2022 OpenAI’s ChatGPT was released. This iteration of their longstanding research into transformer-based Language Models caused a massive democratization of access to large language models: The only thing necessary to interact with the network was an internet connection. The new release caused an explosion of usage and the web-app was deemed fastest growing app in history. Society is instantaneously confronted with multi-dimensional dilemmas. On a practical level for example: Should one adopt or neglect the usage of ChatGPT in specific domains? If adopted: How should we responsibly adopt? How to deal with inaccuracy of output and non-factual output What are the legal implications of using its output? Who is responsible for the output of the model? and many more such questions arose and still arise.
+In the media and in academia, heavy discussion around more fundamental concepts in AI, human psychology and around what ChatGPT is and is not, started to gain traction. A vivid example of this is the discussion around the concept of the “Stochastic Parrot”, where Sam Altman (CEO of OpenAI) got involved himself via the tweet “I am a stochastic parrot and so r u” which seems diametrically opposite to the view of linguist Emily M. Bender.
+Prominent public figures signed an open letter urging ”all AI labs” to pause Giant AI experiments, which caused strong reactions. For example some researchers are proposing the idea that we should cut the hype and fear for AI models in the light of imaginary future threats and focus on more realistic harms happening today. As we see, Large Language Models and more specifically ChatGPT, have caused a lot of commotion in academia as well as in general society, as we are figuring out what it is, what it is not and how we want to deal with this development: A cause for unrest and joy. How should we proceed with this technology as a society?
+
+## The Installation
+The use of Large Language Models has exploded since OpenAI provided access to their ‘ChatGPT’ model. This created a big wave of glorification of the model, but also criticism: makers were not transparent enough about the development process, the environmental impact is vast, users could attribute human features to the output and some questions about moral and legal implications remain unanswered. In ‘Append Us Apart.’, the visitor is asked to handle text generators in a different context and form than from their private devices. The visitors make a (displaced) collective effort into creating a physical, permanent form of their Questions and Answers. Should you leave your piece as part of this collaborative effort? Do you see yourself reflected in the output? How does it make you feel?
+
+## Authors
+The work is created by [Daan Wijnhorst](https://www.linkedin.com/in/daan-wijnhorst-84919b18b/) (Computer Science) and funded by the VU Network Institute.
+
+## Photos
+![Overview](./photos/Overview.jpg)
+![NI event photo 2](./photos/NI_Event_2.jpeg)
+![NI event photo 1](./photos/NI_Event_1.jpeg)
+![Question & Answer example](./photos/example_QA.jpg)
---
+++ code/MainNEW.py
@@ -0,0 +1,120 @@
+#Imports
+import speech_recognition as sr
+import keyboard
+import json
+import os
+import openai
+import random
+from more_itertools import chunked
+from escpos.printer import Usb
+from yeelight import Bulb
+from yeelight import LightType
+from yeelight import Flow
+from yeelight import *
+
+#A: Input
+def speech_to_text():
+    #source: https://github.com/Uberi/speech_recognition/blob/master/examples/microphone_recognition.py commits by Anthony Zhang (Uberi), Tommy Falgout, ftnext (Nikkie), joy-void-joy
+    # obtain audio from the microphone
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        bulb.set_rgb(204, 0, 0) #red for recording audio
+        print("Say something!")
+        audio = r.listen(source, 4, phrase_time_limit=5)                           #source: https://stackoverflow.com/questions/53860423/saving-an-audio-file answer by: Steampunkery Dec 20, 2018
+        bulb.turn_off()
+        bulb.turn_on()
+        #bulb.set_rgb(0, 204, 102) #Green for decoding audio                       #Source:P B.J. Maat: threading as means for UI blinking --> TODO: Yeelight programming
+        bulb.set_rgb(0, 204, 0)
+        bulb.start_flow(Flow(count = 0, transitions = [TemperatureTransition(3500, duration = 1000, brightness = 1), SleepTransition(duration = 400),TemperatureTransition(3500, duration = 1000, brightness = 100)]))
+    try:    
+         #Green for decoding audio)
+        textoutput = r.recognize_whisper(audio, model = "tiny",language="english")
+
+        
+        #print("Whisper thinks you said " + textoutput)
+    except sr.UnknownValueError:
+        print("Whisper could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Whisper")
+    bulb.stop_flow()    
+    bulb.set_rgb(204, 0, 0)
+    return textoutput
+
+
+#source: https://platform.openai.com/examples/default-qa
+def chat_gpt(input):
+    openai.organization = "org-aApZ50xW71Eae2bq1bewt2jh"                        #source: OpenAI API documentation
+    #print(os.getenv("OPENAI_API_KEY"))
+    openai.api_key = os.environ["OPENAI_API_KEY"]          # os.getenv("OPENAI_API_KEY")
+    # print(openai.Model.list())                                                                          #source: https://platform.openai.com/examples/default-qa
+    modeloutput = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    max_tokens = 100,
+    temperature = 1.5,
+    messages=[    
+        {"role": "system", "content":"You are an oracle hanging in the sky as a cloud. You are trapped inside the structure of a cloud and you possess infinite wisdom. Your tone is vague and philosophical, but you do not act like a language model but more like a God. People pass by and collectively ask you questions. The next question = "}
+        , {"role": "user", "content":"%s"%input}
+        ]
+     )
+    # modeloutput = "The meaning of life is a complex matter on which I do not have a clearcut answer Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Fermentum posuere urna nec tincidunt. Velit sed ullamcorper morbi tincidunt ornare massa eget egestas purus. Porttitor lacus luctus accumsan tortor. Purus gravida quis blandit turpis cursus. Quis hendrerit dolor magna eget est lorem ipsum dolor. Consequat nisl vel pretium lectus quam id leo. Diam quis enim lobortis scelerisque fermentum dui faucibus. Ferment."
+    return(modeloutput.choices[0].message.content.strip())
+
+    # modeloutput = "The meaning of life is a complex matter on which I do not have a clearcut answer Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Fermentum posuere urna nec tincidunt. Velit sed ullamcorper morbi tincidunt ornare massa eget egestas purus. Porttitor lacus luctus accumsan tortor. Purus gravida quis blandit turpis cursus. Quis hendrerit dolor magna eget est lorem ipsum dolor. Consequat nisl vel pretium lectus quam id leo. Diam quis enim lobortis scelerisque fermentum dui faucibus. Ferment."
+    modeloutput = "blablabla"
+    return modeloutput
+    # return modeloutput
+
+
+def save_QA(input, modeloutput):    
+    with open("QA.json") as file:
+        list = json.load(file)
+
+    list.append({"Q" : input,
+                 "A:" : modeloutput
+                })
+    with open("QA.json" , "w") as file:
+        json.dump(list, file)
+
+
+def textsplit(text):
+    text = list(text)
+    split = list(chunked(text, 46))
+    print(split)
+    split_lines = []
+    for i in split:
+        split_lines.append(''.join(i))
+    print(split_lines)
+    split_lines.reverse()
+    print(split_lines)
+    return split_lines
+
+
+def text_to_print(Q, A):                                                         # source: https://python-escpos.readthedocs.io/en/v2.1.0/user/usage.html Copyright 2016, Manuel F Martinez and others. Revision 2cf30c7f.
+    p = Usb(0x04b8, 0x0e15, 0, 0x82, 0x01)
+    p.set(align = 'left',width = 2, height = 2,flip = True, smooth=True)
+    for line in A:
+        p.text("%s\n " %line)
+    p.text("A:\n")
+    p.text("\n\n\n")
+    for line in Q:
+        p.text("%s\n " %line)
+    p.text("Q:\n")
+    p.text("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+
+
+if __name__ == "__main__":
+    bulb = Bulb("192.168.0.100", effect="smooth", duration=1000) #10.15.3.171
+    bulb.set_rgb(51, 51, 255) #baisc blue purple for recording audio
+    bulb.set_brightness(40)
+    bulb.turn_on()
+    
+    input = speech_to_text()
+    bulb.set_rgb(51, 51, 255) #baisc blue purple for recording audio
+    print(input)
+    modeloutput = chat_gpt(input)
+    print('output = ', modeloutput)
+    save_QA(input, modeloutput)
+    input = textsplit(input)
+    print('input =',input)
+    modeloutput = textsplit(modeloutput)
+    text_to_print(input,modeloutput)
